5663b8911fcb9e87ff65ae0ef2e700dbeea55fe1
Brandon McClure <brandonmcclure89@gmail.com>
2024-03-04 12:16:46 -0700

    install sql blitz and Ola's maintenance scripts

--- src/sql/init.sh
+++ src/sql/init.sh
@@ -11,3 +11,11 @@ done
 echo "SQL UP, creating databases!"
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -V16 -U sa -P $MSSQL_SA_PASSWORD -l 300 -d master -i /usr/bin/CreateDBs.sql
+sleep 10s
+
+echo "DBS created, installing Ola Hallengren maintenance solution"
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -V16 -U sa -P $MSSQL_SA_PASSWORD -l 300 -d dba -i /usr/bin/MaintenanceSolution.sql
+
+echo "DBS created, installing Brent Ozar First Responder Kit"
+/opt/mssql-tools/bin/sqlcmd -S localhost -V16 -U sa -P $MSSQL_SA_PASSWORD -l 300 -d dba -i /usr/bin/Install-All-Scripts.sql
